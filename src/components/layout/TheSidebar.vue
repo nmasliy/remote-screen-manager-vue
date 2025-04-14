6748b2d02446f7179effa5b7004e6558583ef244
Николай Маслий <n.masliy@friendlee.ru>
2025-04-14 08:52:05 +0500

    feat: update sidebar layout

--- src/components/layout/TheSidebar.vue
+++ src/components/layout/TheSidebar.vue
@@ -2,17 +2,17 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'TheSidebar'
+  name: 'TheSidebar',
 })
 </script>
 
 <template>
-  <v-navigation-drawer>
-    <v-btn to="/" text="Home"></v-btn>
-    <v-btn to="/auth" text="Auth"></v-btn>
+  <v-navigation-drawer class="py-4 px-4">
+    <div class="d-flex flex-column">
+      <v-btn class="mb-4" to="/" text="Home"></v-btn>
+      <v-btn to="/profile" text="Profile"></v-btn>
+    </div>
   </v-navigation-drawer>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
